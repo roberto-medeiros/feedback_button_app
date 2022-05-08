e11e9e6fb881ddcdda6d32b6174c358fd4b6dc9d
User <User@MacBook-Pro-de-MACBOOK.local>
2022-05-07 22:28:08 -0300

    Deploy Server Railway

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,14 @@ const app = express()
 
 app.use(cors())
 
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
+app.use(
+  express.urlencoded({
+    limit: '10mb',
+    extended: true,
+    parameterLimit: 100000
+  })
+)
 
 app.use(routes)
 
